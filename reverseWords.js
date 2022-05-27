893f959fc6b26b0eabdecfd10db32e5ee11a6416
Wayne Van Der Berg <102765856+Waynevdb@users.noreply.github.com>
2022-05-27 14:08:59 +0200

    Create reverseWords.js

---
+++ reverseWords.js
@@ -0,0 +1,9 @@
+//Complete the function that accepts a string parameter, and reverses each word in the string. 
+//All spaces in the string should be retained.
+//EXAMPLE: "This is an example!" ==> "sihT si na !elpmaxe"
+
+
+function reverseWords(str) {
+  let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
+  return reverseWordArr.join(" ")
+}
