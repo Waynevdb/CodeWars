9839e4c4d0400d3656cc6efc53addb6944c1ee90
Wayne Van Der Berg <102765856+Waynevdb@users.noreply.github.com>
2022-05-30 14:29:07 +0200

    Create stringRepeat.js

---
+++ stringRepeat.js
@@ -0,0 +1,9 @@
+//Write a function called repeatStr which repeats the given string string exactly n times.
+//Parameters: Number, String
+//Returns: string(repeated)
+//Example: repeatStr(4, 'Hi') HiHiHiHi
+
+
+function repeatStr (n, s) {
+  return(s.repeat(n))
+}
